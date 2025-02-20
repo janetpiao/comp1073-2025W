bb80e86b194887c9d7ca7f1a9cac4a6fd2c4e700
janetpiao <janetpiao66@gmail.com>
2025-02-19 19:52:54 -0500

    Add a random story generate function. Separated story generate function(old) and random story generate function(new)

--- assignment1/js/story.js
+++ assignment1/js/story.js
@@ -1,20 +1,55 @@
-const output = document.querySelector("#story");
+// GENERATE A STORY FUNCTION
 
-// create variables for each element
+// create variables for elements
+const output1 = document.querySelector("#story");
 const generateButton = document.querySelector("#generateButton");
-const randomStory = document.querySelector("#randomStory");
 
-// add event listener
+// add event listener for generateButton
 generateButton.addEventListener("click", generateStory);
 
-// create generateStory function
+// create generateStory function for generateButton
 function generateStory() {
   // check if this function is working
   console.log("generateStory() function called!");
+
+  // generate a story
   let choice1 = document.getElementById("characters").value;
   let choice2 = document.querySelector("#actions").value;
   let choice3 = document.querySelector("#places").value;
 
-  // create a random story
-  randomStory.textContent = `Once upon a time, there was a ${choice1} ${choice2} in a ${choice3}...`;
+  // output the story
+  output1.textContent = `Once upon a time, there was a ${choice1} ${choice2} in a ${choice3}...`;
+}
+
+// GENERATE A RANDOM STORY FUNCTION
+
+// create variables for elements
+const output2 = document.querySelector("#randomStory");
+const randomButton = document.querySelector("#randomButton");
+let characters = ["cat", "dog", "robot", "wizard"];
+let actions = ["jumps", "runs", "dances", "sings"];
+let places = ["forest", "castle", "beach", "mountain"];
+
+// add event listener for randomButton
+randomButton.addEventListener("click", generateRandomStory);
+
+// create randomElement function
+// Get help from chatGPT and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random to create a randomElement function
+function randomElement(array) {
+  let randomInt = Math.floor(Math.random() * array.length);
+  return array[randomInt];
+}
+
+// create generateRandomStory function for randomButton
+function generateRandomStory() {
+  // check if this function is working
+  console.log("generateRandomStory() function called!");
+
+  // generate a random story
+  let choice1 = randomElement(characters);
+  let choice2 = randomElement(actions);
+  let choice3 = randomElement(places);
+
+  // output the story
+  output2.textContent = `Once upon a time, there was a ${choice1} ${choice2} in a ${choice3}...`;
 }
