4faf93155c132d9deb47ccf861642af1c5c8e0da
janetpiao <janetpiao66@gmail.com>
2025-03-21 14:17:42 -0400

    Built the populateHeader() function

--- lesson-10/js/json.js
+++ lesson-10/js/json.js
@@ -7,7 +7,7 @@ async function populate() {
   // Introducing JavaScript Object Notation (JSON): https://json.org/
   // STEP 4: Store the URL of a JSON file in a variable */
   const requestURL =
-    "https://smccrindle.github.io/comp1073lesson10/js/i-scream.json";
+    "https://smccrindle.github.io/comp1073/lesson10/js/i-scream.json";
   // STEP 5: Use the new URL to create a new request object
   const request = new Request(requestURL);
   // STEP 6: Make a network request with the fetch() function, which returns a Response object
@@ -17,16 +17,20 @@ async function populate() {
   // STEP 8: Output the iScream JSON object to the console
   console.log(iScream);
   // STEP 9a: Invoke the populateHeader function here, then build it below
+  populateHeader(iScream);
   // STEP 10a: Invoke the showTopFlavors function here, then build it below
 }
 // STEP 3b: Call the populate() function
 populate();
 
 /* STEP 9b: Build out the populateHeader() function */
-function populateHeader() {
+function populateHeader(jsonObj) {
   // Create the H1 element
+  let headerH1 = document.createElement("h1");
   // Grab the company name from the JSON object and use it for the text node
+  headerH1.textContent = jsonObj.companyName;
   // Inject the complete H1 element into the DOM, inside the HEADER
+  header.appendChild(headerH1);
 }
 /* STEP 10b: Assemble the showTopFlavors() function */
 function showTopFlavors() {
